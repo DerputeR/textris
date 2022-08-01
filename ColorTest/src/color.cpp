da8fb3ef228ac51d19dc10790e633073dbdb4447
DerputeR <dekxy-developer@yahoo.com>
2022-07-31 23:24:07 -0400

    WriteConsole does not work with ASCII colors

--- ColorTest/src/color.cpp
+++ ColorTest/src/color.cpp
@@ -9,29 +9,17 @@ struct Color
     int b;
 };
 
-std::string GetFGColorStr(const Color& fgColor, const Color& bgColor)
+std::string GetColorStr(const Color& fgColor, const Color& bgColor)
 {
     std::string fgStr = "\x1b[38;2;" + std::to_string(fgColor.r) + ";" + std::to_string(fgColor.g) + ";" + std::to_string(fgColor.b) + "m";
-    return fgStr;
-}
-
-std::string GetBGColorStr(const Color& fgColor, const Color& bgColor)
-{
     std::string bgStr = "\x1b[48;2;" + std::to_string(bgColor.r) + ";" + std::to_string(bgColor.g) + ";" + std::to_string(bgColor.b) + "m";
-    return bgStr;
-}
-
-std::string GetColorString(const Color& fgColor, const Color& bgColor)
-{
-    std::string bgStr = "\x1b[48;2;" + std::to_string(bgColor.r) + ";" + std::to_string(bgColor.g) + ";" + std::to_string(bgColor.b) + "m";
-    std::string fgStr = "\x1b[38;2;" + std::to_string(fgColor.r) + ";" + std::to_string(fgColor.g) + ";" + std::to_string(fgColor.b) + "m";
     return fgStr + bgStr;
 }
 
 std::wstring GetColorWStr(const Color& fgColor, const Color& bgColor)
 {
-    std::wstring bgStr = L"\x1b[48;2;" + std::to_wstring(bgColor.r) + L";" + std::to_wstring(bgColor.g) + L";" + std::to_wstring(bgColor.b) + L"m";
     std::wstring fgStr = L"\x1b[38;2;" + std::to_wstring(fgColor.r) + L";" + std::to_wstring(fgColor.g) + L";" + std::to_wstring(fgColor.b) + L"m";
+    std::wstring bgStr = L"\x1b[48;2;" + std::to_wstring(bgColor.r) + L";" + std::to_wstring(bgColor.g) + L";" + std::to_wstring(bgColor.b) + L"m";
     return fgStr + bgStr;
 }
 
@@ -51,43 +39,52 @@ int main()
     DWORD dwMode = 0;
     GetConsoleMode(hOut, &dwMode);
 
-    dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+    dwMode |= ENABLE_PROCESSED_OUTPUT | ENABLE_VIRTUAL_TERMINAL_PROCESSING;
     SetConsoleMode(hOut, dwMode);
     
-
+    /*
     for (int r = 0; r < 256; r++)
     {
         for (int g = 0; g < 256; g++)
         {
             for (int b = 0; b < 256; b++)
             {
-                wchar_t wBlock[] = L"%s█_%s";
-                std::string fgStr = GetFGColorStr({ r,g,b }, { 0,0,0 });
-                std::string bgStr = GetBGColorStr({ r,g,b }, { 0,0,0 });
+                wchar_t wBlock[] = L"%lsTEST%ls";
+                // wchar_t wBlock[] = L"TEST";
+                std::wstring fgStr = GetFGColorWStr({ r,g,b }, { 0,0,0 });
+                std::wstring bgStr = GetBGColorWStr({ r,g,b }, { 0,0,0 });
 
                 // https://stackoverflow.com/questions/2342162/stdstring-formatting-like-sprintf
                 // Below is a very raw, unsafe version of the above post
-                int fSize = swprintf(
+                int fSize = swprintf( 
                     nullptr,
                     0,
                     wBlock,
                     fgStr, bgStr
                 ) + 1; // extra space for the would-be '\0'
+                
                 swprintf(
                     &wBlock[0],
                     fSize,
                     wBlock,
                     fgStr, bgStr
-                ); // null char is still written, but now we know exactly where it is
+                ); // null char is still written, but now we know exactly where it 
+                
 
                 // ReplaceChars(screen_data, wchar_label, screen_width - (format_size - 1), format_size - 1);
                 // wprintf((GetColorWStr({ r, g, b }, { 0, 0, 0 }) + L"██").data());
                 // wprintf(L"%s", "██");
-                WriteConsole(hOut, wBlock, fSize, NULL, NULL);
+                LPDWORD d = 0;
+                WriteConsole(hOut, wBlock, fSize, d, NULL);
             }
         }
     }
-    // printf((GetColorString({0, 255, 255}, {10, 5, 60}) + "Hello World\n").data());
+
+    */
+    printf((GetColorStr({0, 255, 255}, {10, 5, 60}) + "Hello World\n" + GetColorStr({ 204, 204, 204 }, { 12, 12, 12 })).data());
+    wprintf((GetColorWStr({ 0, 255, 255 }, { 10, 5, 60 }) + L"Hello World\n" + GetColorWStr({ 204, 204, 204 }, { 12, 12, 12 })).data());
+
+    WriteConsoleW(hOut, (GetColorStr({ 0, 255, 255 }, { 10, 5, 60 }) + "Hello World\n").data(), 100, NULL, NULL);
 
     system("pause");
 }
